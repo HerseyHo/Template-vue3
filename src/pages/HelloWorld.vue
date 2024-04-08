68509e707fc3725ae6945eac9c9885641186cace
HerseyHo <hersey_ho@outlook.com>
2024-04-09 05:18:57 +0800

    update template

---
+++ src/pages/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="Wrap">
+    hello
+  </div>
+</template>
+
+<style scoped>
+.Wrap {
+  color: rgb(49, 165, 49);
+}
+</style>
